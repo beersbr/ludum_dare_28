8ffc2fb2b2dabaf0b8ae54bb1aa7c6ceb28d57ff
Super Heavy <hd2319@gmail.com>
2013-12-15 03:43:44 -0800

    Removing directory that shouldn't be there.

--- IEntity.cpp
+++ IEntity.cpp
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 #include "IEntity.h"
-
-=======
 //
 //  IEntity.cpp
 //  ludum_dare_28
@@ -10,5 +7,3 @@
 //  Copyright (c) 2013 Brett Beers. All rights reserved.
 //
 
-#include "IEntity.h"
->>>>>>> 02ff996c575927e8ddbddcfad506983bd3e03280
